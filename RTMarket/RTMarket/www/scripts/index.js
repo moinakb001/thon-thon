27e5b1c4fb820e1b80cd85aa01d9871bc6519ecb
Moinak Bhattacharyya <mb.totaldomination@gmail.com>
2015-10-24 11:04:57 -0700

    angular update

--- RTMarket/RTMarket/www/scripts/index.js
+++ RTMarket/RTMarket/www/scripts/index.js
@@ -4,15 +4,29 @@
 // and then run "window.location.reload()" in the JavaScript Console.
 (function () {
     "use strict";
-    var client = new WindowsAzure.MobileServiceClient(
-    "https://rtmarker.azure-mobile.net/",
-    "lsFNDUxmyAiNzEpBFDPjGwkogMUgQx91"
-);
-    client.login('google');
+    var market = angular.module('market', ['ngRoute']).config(['$routeProvider', function ($routeProvider) {
+        // Routes will be here
+    }]);
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
+        var products = {};
+        var client = new WindowsAzure.MobileServiceClient('https://rtmarker.azure-mobile.net/', 'lsFNDUxmyAiNzEpBFDPjGwkogMUgQx91');
+
+        client.login('google').then(function () {
+            
+            products = client.getTable('products');
+            
+            market.controller('newProd', ['$scope', function ($scope) {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    products.insert({ name: $scope.name, latitude: position.coords.latitude, longitude: position.coords.longitude, description: $scope.description, lifespan: $scope.lifespan });
+                })
+                
+
+
+            }]);
+        }, function (error) { });
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
         
