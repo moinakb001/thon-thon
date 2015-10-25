4e350a560c3ae53b4004a21736951179c01eced6
unbrace3 <unbrace3@gmail.com>
2015-10-25 16:55:21 -0700

    Modals
    use modals to submit listings.
    screw angularjs

--- UI/tabs.js
+++ UI/tabs.js
@@ -1,22 +1,29 @@
-angular.module('TabsApp', [])
-.controller('TabsCtrl', ['$scope', function ($scope) {
-    $scope.tabs = [{
-            title: 'One',
-            url: 'one'
-        }, {
-            title: 'Two',
-            url: 'two'
-        }, {
-            title: 'Three',
-            url: 'three.tpl.html'
-    }];
+var baymsApp = angular.module('baymsApp', ['ng-context-menu']);
 
-    $scope.currentTab = 'one';
+// Authentication & Tabs
+baymsApp.controller('baymsController', function($scope) {
+   $scope.required = {"done": $('input:invalid').length <= 0};
+   $scope.auth = {}; // Sent to server with almost every request
+   $scope.user_type = sessionStorage.getItem('user_type');
+   if (sessionStorage.getItem('user_name')) {
+      // Traditional username/password authentication
+      $scope.auth.user_name = sessionStorage.getItem('user_name');
+      $scope.auth.user_pass = sessionStorage.getItem('user_pass');
+   }
+   else if (sessionStorage.getItem('google_token')) {
+      // Google Sign-In authentication
+      $scope.auth.google_token = sessionStorage.getItem('google_token');
+   }
 
-    $scope.onClickTab = function (tab) {
-        $scope.currentTab = tab.url;
-    }
-    
-    $scope.isActiveTab = function(tabUrl) {
-        return tabUrl == $scope.currentTab;
+   // Restore current tab (or set to default)
+   if ($scope.user_type > 0)
+       $scope.tab = 1;
+   else
+       $scope.tab = 0; 
+   if (sessionStorage.getItem('tab'))
+      $scope.tab = sessionStorage.getItem('tab');
+   // Save the current tab in sessionStorage
+   $scope.$watch('tab',function(){
+      sessionStorage.setItem('tab', $scope.tab);
+   });
 });
